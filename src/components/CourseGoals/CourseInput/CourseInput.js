30cdf5790d4cd9f7e486834b31b49e98bf51faad
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-23 00:52:00 -0400

    75. Setting Dynamic Inline Styles
    - Create an `isValid` state that tracks whether the input contains any data
      when the user clicks the submit button.
    
    - If the input is empty when the user clicks the button, set the "Course Goal"
      text to red, as well as the border and background color of the input, and don't
      create the goal, of course.  The color settings are done with dynamic inline
      styling.
    
    - Reset the red colors back to black if the user begins to enter data in the
      input.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input
+          style={{
+            borderColor: !isValid ? 'red' : 'black',
+            backgroundColor: !isValid ? 'salmon' : 'transparent',
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
