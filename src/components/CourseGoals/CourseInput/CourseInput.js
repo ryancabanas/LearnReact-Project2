81de1d69bcbba7c0d598e99d03d9485cb35880f5
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-23 22:49:33 -0400

    78. Styled Components & Dynamic Props
    - Created a styled `div` component named `FormControl` inside of the same file
      as another component, which is fine to do, and may make sense, if the main
      component of this file is the only place that this additional component is
      being used.
    
    - Note that, again, in the style `div` component syntax, when sticking the CSS
      there in the tagged template literal, we replace all previous class names
      with the `&` symbol.
    
    - Note that we still needed to dynamically inject the `invalid` CSS class name
      into our styled `div` component to control the styling for the error. Doing it
      this way is ***one*** way of doing this.  The instructor is about to show
      another way of doing this.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+// Adding an additional componenet to this one JS file.
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: lightpink;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -27,10 +62,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl className={!isValid && 'invalid'}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
