71904931993b2a91be7d2f48612cd5faba782c24
Ryan Cabanas <ryangithub777@gmail.com>
2022-06-27 21:06:47 -0400

    81. Dynamic Styles with CSS Modules
    - Primary focus of this lesson was working on the `CourseInput.js` using CSS
      modules.
    
      - Change the `CourseInput.css` file name to `CourseInput.module.css`, as
        needed for CSS modules with the `create-react-app`.
    
      - Then we comment out all of the `styled-components` work that was done
        previously inside of the `CourseInput.js`.
    
        - Comment out the import of `style-components`.
        - Comment out the `FormControl` element that was created based off of a
          `div` element.
    
      - Import the new `CourseInput.module.css` module.
    
      - Change the `FormControl` element usage back to a typical `div` element in the
        `CourseInput.js` file and use the new CSS module syntax with the `className`
        property/attribute.  This gets a little tricky when you need to use ***two***
        class names, though.  It also is different when your CSS className includes
        a hyphen.  Here's what was done.
    
        - To access the `form-control` CSS className, you need to use the alternate
          syntax for accessing object properties, which is `styles['form-control']`.
    
        - Additionally, because we're using ***two*** class names, we need to use
          template literals to get the names in there.
    
    - In addition to these changes, we update the `Button.module.css` module with
      the media query styling that we had previously added to the `Button.js`
      module when we were using the `styled-components` approach.  We took this
      media query out of the `Button.js` module and added it to the `Button.module.css`
      module, but we also needed to make a small change to write the syntax in a
      *normal* CSS manner and not the `styled-components` specific syntax manner.  So
      inside of the media query, we needed to call out the `.button` class explicitly.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,35 +1,35 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 // Adding an additional componenet to this one JS file.
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'red' : 'black')};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${props => (props.invalid ? 'red' : 'black')};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-    background-color: ${props => (props.invalid ? 'lightpink' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background-color: ${props => (props.invalid ? 'lightpink' : 'transparent')};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -55,10 +55,12 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
